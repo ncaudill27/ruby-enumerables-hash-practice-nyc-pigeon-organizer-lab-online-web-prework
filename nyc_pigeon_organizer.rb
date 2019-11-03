3e3677162f7b870d118dfec153f77b790e1841e0
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-03 05:28:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,9 @@
+def names_for_keys(data)
+  # These will be attributes in new structure
+  level_one = data.collect{ |memo, (key, value)| memo }
+  p data.each{data[level_one]}
+end
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names_for_keys(data)
 end
